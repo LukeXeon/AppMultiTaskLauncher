41a3799194bd0fad95ddc6635e4828a9063fc56e
luotinghui <luotinghui@bilibili.com>
2021-12-11 02:11:20 +0800

    m

--- multitask/src/main/java/open/source/multitask/RemoteTaskException.kt
+++ multitask/src/main/java/open/source/multitask/RemoteTaskException.kt
@@ -14,6 +14,7 @@ class RemoteTaskException(
     )
 
     override fun fillInStackTrace(): Throwable {
+        super.fillInStackTrace()
         stackTrace = stackTraceElements
         return this
     }
