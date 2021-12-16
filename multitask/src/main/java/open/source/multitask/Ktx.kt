54c7725b952915ea147cc2e6fec8b1201fef0abb
luotinghui <luotinghui@bilibili.com>
2021-12-16 18:17:38 +0800

    update

--- multitask/src/main/java/open/source/multitask/Ktx.kt
+++ multitask/src/main/java/open/source/multitask/Ktx.kt
@@ -2,12 +2,7 @@ package open.source.multitask
 
 import android.os.SystemClock
 import android.util.Log
-import kotlin.reflect.KClass
 
-
-internal fun MutableMap<KClass<out TaskExecutor>, TaskInfo>.add(task: TaskInfo) {
-    put(task.type, task)
-}
 
 internal suspend fun <T> trace(tag: String, message: String, action: suspend () -> T): T {
     val start = SystemClock.uptimeMillis()
